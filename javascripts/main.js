36011a80b8b1544593b669e31798a4ab818f9468
imakawa <bretwang@yeah.net>
2015-07-15 00:30:20 +0900

    local data storage

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,7 @@
 console.log('This would be the main JS file.');
+var blogApp = angular.module('blogApp', []);
+blogApp.controller('headerCtrl',function($http,$scope){
+  $http.get("data/test.json").then(function(data) {
+        $scope.data = data;
+    });
+});
